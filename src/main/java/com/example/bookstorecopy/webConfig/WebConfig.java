64d16df4297f9bf53dbca845b2dc1cb57b4ddfd4
Sithu Han <110212399+ArtherPerry@users.noreply.github.com>
2024-10-28 10:34:28 +0700

    Update WebConfig.java

--- src/main/java/com/example/bookstorecopy/webConfig/WebConfig.java
+++ src/main/java/com/example/bookstorecopy/webConfig/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
                 .allowedOrigins("https://images.unsplash.com") // Specify allowed origins (image host)
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials if needed
