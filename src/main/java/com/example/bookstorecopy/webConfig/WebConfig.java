5968a70f760a6b69bd5d50a0e0cd090b07621492
Sithu Han <110212399+ArtherPerry@users.noreply.github.com>
2024-10-28 10:47:42 +0700

    Update WebConfig.java

--- src/main/java/com/example/bookstorecopy/webConfig/WebConfig.java
+++ src/main/java/com/example/bookstorecopy/webConfig/WebConfig.java
@@ -5,17 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("https://images.unsplash.com") // Specify allowed origins (image host)
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*") // Allow all origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials if needed
     }
 }
-
